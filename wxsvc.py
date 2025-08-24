066bf88260269ab21ed314ff2823b44e170dff79
Previo Prakasa <previo.prakasa@gmail.com>
2025-08-24 22:00:11 +0700

    working audio fetch/post

--- wxsvc.py
+++ wxsvc.py
@@ -1,8 +1,16 @@
+import logging
 import json
+from io import BytesIO
 from flask import Flask, request, render_template
+from whisperx import load_audio
+
+AUDIO_CACHE_PATH = './cache/0'
 
 app = Flask(__name__)
 rc = 0
+logger = logging.getLogger()
+
+print(dir(request))
 
 def incr(d):
   global rc
@@ -16,8 +24,14 @@ def root():
 
 @app.route('/api/trans/a', methods=['POST'])
 def svc():
-  data = dict()
-  incr(data)
-  print(request.files)
-  print(request)
-  return json.dumps(data)
+  resp = dict()
+  file = request.files.get('file', None)
+  if file is None:
+    logger.error('file recv is null')
+    return {}
+  with open(AUDIO_CACHE_PATH, 'wb') as fp:
+    fp.write(file.read())
+  audio = load_audio(AUDIO_CACHE_PATH)
+  ...
+  incr(resp)
+  return json.dumps(resp)
