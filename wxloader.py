cb4de67a533ab9ea6d29c7c39f37a97c1289dd07
Previo Prakasa <previo.prakasa@gmail.com>
2025-08-25 15:50:19 +0700

    various

--- wxloader.py
+++ wxloader.py
@@ -4,11 +4,13 @@ import gc
 from utils import download
 
 batch_size = 4
+compute_type = 'int8'
 device = 'gpu' if torch.cuda.is_available() else 'cpu'
 print('using device:', device)
 
 models = dict()
-models['ja'] = whisperx.load_model('base', device, compute_type='int8', language='ja', download_root='./cache')
+models['ja'] = whisperx.load_model('base', device=device, compute_type=compute_type, language='ja', download_root='./cache')
+models['en'] = whisperx.load_model('base', device=device, compute_type=compute_type, language='en', download_root='./cache')
 
 # alinger_models = dict()
 # alinger_metadata = dict()
@@ -22,7 +24,8 @@ def collect_garbage():
 def transcribe(lang, audio):
   global models, batch_size
   # diarized_seg = diarizer_model(audio)
-  res = models[lang].transcribe(audio, batch_size=batch_size, task='translate')
+
+  res = models[lang].transcribe(audio, batch_size=batch_size, task='transcribe' if lang == 'en' else 'translate')
   # res = whisperx.align(res['segments'], alinger_models[lang], alinger_metadata[lang], audio, device)
   # res = whisperx.assign_word_speakers(diarized_seg, res)
   return res
@@ -36,4 +39,8 @@ if __name__ == '__main__':
   }
   download(test_audios[1])
   audio = whisperx.load_audio(test_audios[1]['path'])
-  print(transcribe('ja', audio))
+  result = transcribe('ja', audio)
+  print(result)
+  with open('./cache/tests/audio_lesson_01.json', 'w') as fp:
+    import json
+    json.dump(result, fp, indent=2)
--- wxsvc.py
+++ wxsvc.py
@@ -1,37 +1,50 @@
 import logging
 import json
+from os import makedirs, remove as rm
 from io import BytesIO
+from tempfile import mkstemp
+from time import time
 from flask import Flask, request, render_template
 from whisperx import load_audio
+from wxloader import transcribe
 
-AUDIO_CACHE_PATH = './cache/0'
+
+CACHE_DIR = './cache/atemp'
 
 app = Flask(__name__)
 rc = 0
 logger = logging.getLogger()
+makedirs(CACHE_DIR, exist_ok=True)
 
-print(dir(request))
 
 def incr(d):
   global rc
   rc += 1
   d['rc'] = rc
+  d['ts'] = time()
 
 
 @app.route('/')
 def root():
   return render_template('./home.html')
 
+
 @app.route('/api/trans/a', methods=['POST'])
 def svc():
   resp = dict()
+  incr(resp)
   file = request.files.get('file', None)
   if file is None:
     logger.error('file recv is null')
     return {}
-  with open(AUDIO_CACHE_PATH, 'wb') as fp:
+
+  fd, path = mkstemp(dir=CACHE_DIR)
+  with open(fd, 'wb') as fp:
     fp.write(file.read())
-  audio = load_audio(AUDIO_CACHE_PATH)
-  ...
-  incr(resp)
+  audio = load_audio(path)
+  rm(path)
+  resp['trans'] = list()
+  resp['trans'].append(transcribe('ja', audio))
+  # resp['trans'].append(transcribe('en', audio))
+  print(resp['trans'])
   return json.dumps(resp)
